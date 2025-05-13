49466d6b25d7508a862b14ab119ebcabccdada2f
zakibn18 <jekben815@gmail.com>
2025-05-13 10:09:32 +0700

    pembuatan node dan pemberian value untuk data noMhs dan name

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -14,6 +14,17 @@ struct Node
 // Deklarasi poiter START dan pemberian nilai
 Node *START;
 
+// Deklarasi prosedur addNode
+void addNode()
+{
+  //pembuatan node dan pemberian value untuk data noMhs dan name
+  Node *newNode = new Node(); // step 1 Buat node baru
+  cout << "\nEnter the roll number if the student : ";
+  cin >> newNode->noMhs;
+  cout<< "\nEnter the name of the student : ";
+  cin >> newNode->name;
+}
+
 int main()
 {
   
