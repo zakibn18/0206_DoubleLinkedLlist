94da65fbb149e3c65abbbbef1ae8fe9598705501
zakibn18 <jekben815@gmail.com>
2025-05-13 10:02:40 +0700

    dekalrasi pointer next dan pref untuk penunjukan data sebelum dan sesudah

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -6,6 +6,9 @@ struct Node
   // deklarasi noMhs dan name untuk menapung data 
   int noMhs;
   string name;
+  // dekalrasi pointer next dan pref untuk penunjukan data sebelum dan sesudah
+  Node *next;
+  Node * perv;
 };
 
 int main()
